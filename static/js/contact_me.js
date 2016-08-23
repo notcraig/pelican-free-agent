404aa652189bab22b2d4cc2e6607c3fd80a9d415
Fish <github@thetawave.studio>
2016-08-23 15:58:12 -0600

    fixed mail; confirmed working.
    updated contact_me.js with proper url format when using 3rd party
    services
    enabled all form fields in contact_me.php

--- static/js/contact_me.js
+++ static/js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "http://yourdomain.com/mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
--- static/mail/contact_me.php
+++ static/mail/contact_me.php
@@ -2,7 +2,7 @@
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
-  // empty($_POST['phone']) 		||
+  empty($_POST['phone']) 		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
@@ -12,7 +12,7 @@ if(empty($_POST['name'])  		||
 
 $name = $_POST['name'];
 $email_address = $_POST['email'];
-//$phone = $_POST['phone'];
+$phone = $_POST['phone'];
 $message = $_POST['message'];
 $url = 'https://api.sendgrid.com/';
 $user = 'username';
